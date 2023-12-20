f44ba8be647f0cabc76aa5da5305969d5242dd52
Zimit121 <zimitzimit122@gmail.com>
2023-12-20 21:00:02 +0530

    fetch1

--- MASAI/fetch1/index3.js
+++ MASAI/fetch1/index3.js
@@ -1,7 +1,38 @@
+function storeData() {
+    const name = document.getElementById('name').value;
+    const age = document.getElementById('age').value;
 
-function storingData(name,age){
-    console.log("Name:",name)
-    console.log("Age :",age);
+    if (name && age) {
+        const userData = { name, age };
+        localStorage.setItem('userData', JSON.stringify(userData));
+
+        // Clear form fields after storing data
+        document.getElementById('userDataForm').reset();
+    } else {
+        alert('Please enter both name and age.');
+    }
 }
 
-storingData("zimit",72)
+function retrieveData() {
+    const userDataString = localStorage.getItem('userData');
+
+    if (userDataString) {
+        const userData = JSON.parse(userDataString);
+
+        const displayArea = document.getElementById('displayArea');
+        displayArea.innerHTML = `
+            <table>
+                <tr>
+                    <th>Name</th>
+                    <th>${userData.name}</th>
+                </tr>
+                <tr>
+                    <td>Age</td>
+                    <td>${userData.age}</td>
+                </tr>
+            </table>
+        `;
+    } else {
+        alert('No data found in Local Storage.');
+    }
+}
